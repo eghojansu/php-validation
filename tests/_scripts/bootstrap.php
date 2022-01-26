5288d783304da9e264829550094bcbb37ef16e84
Eko Kurniawan <setiawanegho@gmail.com>
2022-01-26 09:10:57 +0700

    Idealism

--- tests/_scripts/bootstrap.php
+++ tests/_scripts/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-define('TEST_PROJECT', dirname(dirname(__DIR__)));
-define('TEST_ROOT', dirname(__DIR__));
+define('TEST_PROJECT', realpath(__DIR__ . '/../..'));
+define('TEST_ROOT', realpath(__DIR__ . '/..'));
 define('TEST_DATA', TEST_ROOT . '/_data');
