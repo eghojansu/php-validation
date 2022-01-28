429e3641d2b71e72f2cd601c6d27f3a780b70c79
Eko Kurniawan <setiawanegho@gmail.com>
2022-01-28 11:00:32 +0700

    update nullable rule

--- src/Rules/Nullable.php
+++ src/Rules/Nullable.php
@@ -2,13 +2,14 @@
 
 namespace Ekok\Validation\Rules;
 
+use Ekok\Utils\Val;
 use Ekok\Validation\Rule;
 
 class Nullable extends Rule
 {
     protected function doValidate($value)
     {
-        if (null === $value) {
+        if (Val::isEmpty($value)) {
             $this->context->stopPropagation();
         }
 
